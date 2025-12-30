a9fcdc9ba7ca56f2d4a2c48341f54f90a07caf5b
Priyanshu Barapatre <priyanshu.barapatre@hotelkeyapp.com>
2025-12-30 10:57:16 +0530

    TF changes

--- dev-standard/backend.tf
+++ dev-standard/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
     bucket         = "pb-dev-state-bucket"
-    key            = "dev-standard/terraform.tfstate"
+    key            = "environments/dev/${terraform.workspace}/terraform.tfstate"
     region         = "us-west-2"
-    use_lockfile = "pb-dev-terraform-locks"
+    use_lockfile   = "pb-dev-terraform-locks"
     encrypt        = true
   }
 }
