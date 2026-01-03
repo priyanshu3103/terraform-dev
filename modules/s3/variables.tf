e62b16f62e2b5e29096020e0592532f7d135ccb4
Priyanshu Barapatre <priyanshu.barapatre@hotelkeyapp.com>
2026-01-03 14:22:05 +0530

    Adding bucket in west region

--- modules/s3/variables.tf
+++ modules/s3/variables.tf
@@ -7,7 +7,10 @@ variable "versioning" {
   type        = bool
   default     = true
 }
-
+variable "bucket_id" {
+  description = "S3 bucket ID"
+  type        = string
+}
 variable "tags" {
   type        = map(string)
   default     = {}
--- modules/sns/variables.tf
+++ modules/sns/variables.tf
@@ -7,3 +7,23 @@ variable "tags" {
   type        = map(string)
   default     = {}
 }
+variable "sns_topic_arn" {
+  description = "SNS topic ARN"
+  type        = string
+}
+variable "events" {
+  description = "S3 events to trigger"
+  type        = list(string)
+  default     = ["s3:ObjectCreated:*"]
+}
+variable "filter_prefix" {
+  description = "S3 object key prefix filter"
+  type        = string
+  default     = null
+}
+
+variable "filter_suffix" {
+  description = "S3 object key suffix filter"
+  type        = string
+  default     = null
+}
