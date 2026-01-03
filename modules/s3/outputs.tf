6bc982253d86561d1481549aed3d54354d6b5635
Priyanshu Barapatre <priyanshu.barapatre@hotelkeyapp.com>
2026-01-03 12:31:26 +0530

    Adding bucket in west region

--- modules/s3/outputs.tf
+++ modules/s3/outputs.tf
@@ -1,7 +1,7 @@
 output "bucket_id" {
-  value = aws_s3_bucket.aws-s3-bucket.id
+  value = aws_s3_bucket.s3-bucket.id
 }
 
 output "bucket_arn" {
-  value = aws_s3_bucket.aws-s3-bucket.arn
+  value = aws_s3_bucket.s3-bucket.arn
 }
